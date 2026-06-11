871de9e79f6e4e24e3b0f0fa5b4240e6ff4e81b9
HamimBhai742 <mdhamim5088@gmail.com>
2026-06-11 21:27:43 +0600

    Updated server

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://amer-rokto.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
